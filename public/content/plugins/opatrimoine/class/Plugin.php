6b1068d08a1373453c8a392b715c4c0e990836c4
aurelien-genois <aurelien.genois@gmail.com>
2021-09-21 17:46:47 +0200

    initialize plugin opatrimoine

---
+++ public/content/plugins/opatrimoine/class/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OPatrimoine;
+
+
+class Plugin
+{
+
+    public function __construct() 
+    {
+        
+    }
+
+    public function activate()
+    {
+
+    }
+
+    public function deactivate()
+    {
+        
+    }
+    
+}
---
+++ public/content/plugins/opatrimoine/opatrimoine.php
@@ -0,0 +1,22 @@
+ <?php
+/**
+ * Plugin Name: O'Patrimoine
+ * Author: Promo Ulysse WP
+ * Description: Ce plugin crée les fonctionnalités nécessaires au bon fonctionnement du site O'Patrimoine
+ */
+
+ use OPatrimoine\Plugin;
+
+ require __DIR__ . '/vendor-opatrimoine/autoload.php';
+
+ $plugin = new Plugin();
+
+ register_activation_hook(
+     __FILE__,
+     [$plugin, 'activate'],
+ );
+
+ register_deactivation_hook(
+     __FILE__,
+     [$plugin, 'deactivate'],
+ );
