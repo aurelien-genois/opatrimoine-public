8a1cca359e70248d5a22ff3d74a53878d0622c6b
aurelien-genois <aurelien.genois@gmail.com>
2021-09-21 15:34:36 +0200

    add _install folder with configuration file

---
+++ _install/configuration.sh
@@ -0,0 +1,19 @@
+INSTALL_PATH="/var/www/html/spe-wp/opatrimoine/public";
+
+MYSQL_USER="explorateur"
+MYSQL_PASSWORD="Ereul9Aeng"
+MYSQL_HOST="localhost"
+WORDPRESS_BDD="ulysse_opatrimoine"
+
+
+WORDPRESS_URL="http://localhost/spe-wp/opatrimoine/public"
+
+WORDPRESS_SITE_NAME="OPatrimoine"
+WORDPRESS_ADMIN_NAME="ulysse-opatrimoine"
+WORDPRESS_ADMIN_PASSWORD="ylusse2021"
+WORDPRESS_ADMIN_EMAIL="no-email@mail.com"
+
+# optionnal configurations
+WORDPRESS_FOLDER="wp"
+WORDPRESS_CONTENT_FOLDER="content"
+WORDPRESS_TABLE_PREFIX="opatrimoine_"
