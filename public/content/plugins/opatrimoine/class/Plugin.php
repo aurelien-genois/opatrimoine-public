855d74a6dc805e5e178528be039d89c896b39750
aurelien-genois <aurelien.genois@gmail.com>
2021-09-22 12:29:26 +0200

    init cpt for place and guided-tour

--- public/content/plugins/opatrimoine/class/Plugin.php
+++ public/content/plugins/opatrimoine/class/Plugin.php
@@ -2,13 +2,40 @@
 
 namespace OPatrimoine;
 
-
 class Plugin
 {
+    // cpt definitions
+    public $customPostTypes = [
+        'place' => [
+            'label' => 'Lieux',
+            'icon' => 'dashicons-bank',
+        ],
+        'guided-tour' => [
+            'label' => 'Visites guidées',
+            'icon' => 'dashicons-calendar-alt',
+        ],
+    ];
 
+    // plugin initialization
     public function __construct() 
     {
         
+        add_action(
+            'init',
+            [$this, 'registerCustomPostTypes'],
+        );
+    }
+
+    public function registerCustomPostTypes()
+    {
+        foreach($this->customPostTypes as $identifier => $description) {
+            $postType = new CustomPostType(
+                $identifier,
+                $description['label'],
+                $description['icon'],
+            );
+            $postType->register();
+        };
     }
 
     public function activate()
@@ -20,5 +47,4 @@ class Plugin
     {
         
     }
-    
 }
