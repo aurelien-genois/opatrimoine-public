039552b4560492e76e8e610e56233b9384d85210
aurelien-genois <aurelien.genois@gmail.com>
2021-09-22 12:27:59 +0200

    create cpt class in plugin

---
+++ public/content/plugins/opatrimoine/class/CustomPostType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace OPatrimoine;
+
+class customPostType
+{
+    public $supports = [
+        'title',
+        'thumbnail',
+        'editor',
+        'author',
+    ];
+
+    public $identifier;
+    public $label;
+    public $icon;
+
+    public function __construct($identifier, $label, $icon)
+    {
+        $this->identifier = $identifier;
+        $this->label = $label;
+        $this->icon = $icon;
+        
+    }
+
+    public function register()
+    {
+        $result = register_post_type(
+            $this->identifier,
+            [
+                'label' => $this->label,
+                'public' => true,
+                'hierarchical' => false,
+                'menu_icon' => $this->icon,
+                'supports' => $this->supports,
+                'capability_type' => $this->identifier,
+                'map_meta_cap' => true,
+            ],
+        );
+        
+        flush_rewrite_rules();
+
+        return $result;
+    }
+}
