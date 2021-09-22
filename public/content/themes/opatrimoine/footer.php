a2fd2aec61a4ad74875960a6ed7d9ee6a03d8722
MachadoVazLuana <luana.vaz.infographiste@gmail.com>
2021-09-23 01:47:04 +0200

    Header + Footer Integration

--- public/content/themes/opatrimoine/footer.php
+++ public/content/themes/opatrimoine/footer.php
@@ -1,5 +1,23 @@
 
-    <footer class="section footer">
+    <footer class="section-footer">
+
+    <nav>
+        <div class="social-media">
+         <i class="fab fa-facebook-square"></i>
+         <i class="fab fa-twitter-square"></i>
+         <i class="fab fa-instagram-square"></i>
+        </div>
+
+        <div class="links">
+          <ul>
+            <li><a href="#">Acceuil</a></li>
+            <li><a href="#">Liste des lieux</a></li>
+            <li><a href="#">Infos pratiques</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Mentions légales</a></li>
+          </ul>
+        </div>
+    </nav>
 
     </footer>
 
--- public/content/themes/opatrimoine/header.php
+++ public/content/themes/opatrimoine/header.php
@@ -6,9 +6,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="/spe-wp/opatrimoine/public/content/themes/opatrimoine/style.css" rel="stylesheet">
     <!-- TODO add fonts links -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/45e38e596f.js" crossorigin="anonymous"></script>
+    
 
     <?php wp_head();?>
 </head>
 
 <body>
+
+<header>
+    <nav id="header">
+       
+         <a href="#" class="logo"> <img class="opatrimoine-logo" src="/spe-wp/opatrimoine/public/content/themes/opatrimoine/assets/image/opatrimoinelogo.png" alt="O'Patrimoine logo">
+             <h4>O'Patrimoine</h4> 
+         </a>
+     
+        <div class="links">
+          <ul>
+            <li><a href="#">Liste des lieux</a></li>
+            <li><a href="#">Infos pratiques</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">S'incrire</a></li>
+          </ul>
+        </div>
+        <button id="connection">Connexion</button>
+    </nav>
+</header>
