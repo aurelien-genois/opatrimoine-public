7ee74d3f314bb460ffe7380da530d131740729cc
aurelien-genois <aurelien.genois@gmail.com>
2021-09-24 18:00:24 +0200

    complete vue.config.js

--- public/content/themes/opatrimoine/components/vue.config.js
+++ public/content/themes/opatrimoine/components/vue.config.js
@@ -1,5 +1,28 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
-}
+  ],
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "src/assets/main.scss";`
+      }
+    }
+  },
+
+  // permet de ne pas renommer les noms de dossier n'importe comment dans le fichier /dist
+  filenameHashing: false,
+  publicPath: './',
+
+  chainWebpack: config => {
+    // config.plugins.delete('html');
+    config.plugins.delete('preload');
+    config.plugins.delete('prefetch');
+    config.optimization.delete('splitChunks')
+  },
+  devServer: {
+    inline: false, // https://webpack.js.org/configuration/dev-server/#devserver-inline
+    writeToDisk: true,
+    hot: false,
+  },
+  // productionSourceMap: false
