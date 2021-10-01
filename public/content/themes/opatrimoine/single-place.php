feb080967a3eb3b712fc6554264e2fc7441f95e9
aurelien-genois <aurelien.genois@gmail.com>
2021-10-01 16:07:05 +0200

    fixe the json data send in data- attribute to prevent quotes breaking the json string

--- public/content/themes/opatrimoine/single-place.php
+++ public/content/themes/opatrimoine/single-place.php
@@ -32,7 +32,7 @@ $guidedTours = getGuidedToursByPlaceId(get_the_ID());
     <section class="place-tours">
         <h2>Visites</h2>
         <!-- TODO create the vuetify component -->
-        <div id="tours-calendar" data-vue="<?= json_encode($guidedTours); ?>"></div>
+        <div id="tours-calendar" data-vue="<?= htmlspecialchars(json_encode($guidedTours), ENT_QUOTES, 'UTF-8') ?>"></div>
     </section>
 
     <!-- NTH add comments form -->
