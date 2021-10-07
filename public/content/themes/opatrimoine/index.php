9f1477bf230761667f84a59f294ca6a686fdc16c
aurelien-genois <aurelien.genois@gmail.com>
2021-10-07 17:51:34 +0200

    add dashboard simple layout and logout on home page

--- public/content/themes/opatrimoine/index.php
+++ public/content/themes/opatrimoine/index.php
@@ -1,8 +1,16 @@
 <?php
+if($_GET['logout']) {
+    wp_logout();
+    $logoutMsg = "Vous êtes bien déconnecté";
+}
+
 get_header();
 ?>
 
 
+<?php if(isset($logoutMsg)): ?>
+    <p><?= $logoutMsg ?></p>
+<?php endif ?>
 
 <?php
     get_template_part('partials/carousel');
@@ -10,16 +18,16 @@ get_header();
 <main class="page-content">
 
 
-<section id='presentation'> 
+    <section id='presentation'> 
 
-    <h2>O’Patrimoine, le concept</h2>
-        <p> 
-        O’Patrimoine a pour objectif de centraliser les visites guidées sur une seule plateforme et 
-        de faciliter l’accès, hors Journées du Patrimoine,  à des lieux dont certaines parties sont parfois 
-        inaccessibles au public.
-        </p>
- 
-</section>
+        <h2>O’Patrimoine, le concept</h2>
+            <p> 
+            O’Patrimoine a pour objectif de centraliser les visites guidées sur une seule plateforme et 
+            de faciliter l’accès, hors Journées du Patrimoine,  à des lieux dont certaines parties sont parfois 
+            inaccessibles au public.
+            </p>
+    
+    </section>
 
 
     
--- public/content/themes/opatrimoine/views/dashboard.view.php
+++ public/content/themes/opatrimoine/views/dashboard.view.php
@@ -1,1 +1,20 @@
-<h1>Dashboard, hello <?= wp_get_current_user()->display_name ?></h1>
+<?php
+extract($args);
+
+
+get_header();
+?>
+
+<main class="page-content">
+    <h1><?= $user->display_name ?></h1>
+    <a href="<?= get_home_url() . "/?logout=true"?>">Se déconnecter</a>
+    <!-- NTH link to updtate user informations -->
+
+
+
+</main>
+
+
+<?php
+get_footer();
+?>
