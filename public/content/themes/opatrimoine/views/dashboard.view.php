89b722e2139811925effca0b1163e73b40d67536
MachadoVazLuana <luana.vaz.infographiste@gmail.com>
2021-10-12 15:18:59 +0200

    Add dashboard-page

--- public/content/themes/opatrimoine/views/dashboard.view.php
+++ public/content/themes/opatrimoine/views/dashboard.view.php
@@ -1,1 +1,41 @@
-<h1>Dashboard, hello <?= wp_get_current_user()->display_name ?></h1>
+<?php
+get_header();
+
+$guidedTours = getGuidedToursByPlaceId(get_the_ID());
+?>
+
+<h3><?= wp_get_current_user()->display_name ?></h3>
+
+            <div class="buttons">
+                <a class="button" href="">Réactualiser mes informations</a>
+            
+                <a class="button" href="<?= get_home_url() . "/?logout=true"?>">Déconnexion</a>
+                    
+            </div>
+
+<main class="dashboard-content">
+
+<section class="place-tours">
+        <h2>Mes réservations</h2>
+        <!-- TODO create the vuetify component -->
+        <div id="tours-calendar" data-vue="<?= htmlspecialchars(json_encode($guidedTours), ENT_QUOTES, 'UTF-8') ?>"></div>
+    </section>
+
+    <!-- places-list -->
+    <section class="post-listing">
+    <?php
+    if(have_posts()) {
+        while (have_posts()) {
+            the_post();
+
+            get_template_part('partials/place-thumbnail');
+        }
+    }
+    ?>
+    </section>
+
+</main>
+
+<?php
+get_footer();
+?>
